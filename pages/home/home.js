2e061bae810f5480db24f31c02d91d7e7e35540e
1 <a@1deiMac-4.local>
2018-02-26 16:28:24 +0800

    add tab

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,17 +8,22 @@ Page({
      */
     data: {
         imgUrls: [],
+        newsList: [],
         indicatorDots: true,
         autoplay: true,
         interval: 5000,
-        duration: 3000
+        duration: 3000,
+        baifo: '../img/icon_lifo_new.png',
+        fashi: '../img/icon_event_new.png',
+        fawu: '../img/icon_object_new.png'
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        this.getBanner()
+        this.getBanner(),
+            this.getNews()
     },
 
     /**
@@ -94,13 +99,36 @@ Page({
                     arr.push(url)
 
                 }
-                
-                that.setData({ imgUrls: arr })
 
+                that.setData({ imgUrls: arr })
             }
 
         })
     },
+
+    /**
+     * 获取资讯列表
+     */
+    getNews: function () {
+        var that = this
+        wx.request({
+            url: 'https://staging.baifotong.com/news/news',
+            success: function (res) {
+                console.log('news list === success')
+                const count = res.data.results.length
+
+                var arr = [];
+                var i;
+                for (i = 0; i < count; i++) {
+
+                    const news = res.data.results[i];
+                    arr.push(news)
+                }
+                that.setData({ newsList: arr })
+                console.log(arr)
+            }
+        })
+    }
 
 })
 
